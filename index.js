430c2ce633b0f26999d5a123788b053465e65d98
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-24 02:18:54 +0700

    Display the cards

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ let message = ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: "+ sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
