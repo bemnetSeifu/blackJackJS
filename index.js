2e88e29f065863a00de4d28610a038e722c6a93a
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-23 18:12:55 +0700

    Add the message variable

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true 
 
+let message = ""
+
 if (sum <= 20){
-    console.log("Do you want a new card? 🙂")
+    message = "Do you want a new card? 🙂"
 } else if(sum === 21){
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false
 } 
+console.log(message)
