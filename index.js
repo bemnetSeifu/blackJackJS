2109c69d16abfdfd98fc6b67bf3ffa948184c9e3
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-23 10:28:21 +0700

    Adding hasBlacJack and isAlive Variable

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 let firstCard = 7;
 let secondCard = 2; 
-
-
 let sum = firstCard + secondCard
+
+let hasBlackJack = false
+let isAlive = true 
 
 if (sum <= 20){
     console.log("Do you want a new card? 🙂")
-} else if(sum == 21){
+} else if(sum === 21){
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
-}
+    isAlive = false
+} 
