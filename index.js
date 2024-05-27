3f178c97139dc0707373f55dbcc0c504143cab3e
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-28 02:03:14 +0700

    Modifying startGame and newCard functions

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 
-let firstCard = getRandomCard();
-let secondCard = getRandomCard(); 
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true 
-
+let isAlive = false 
 let message = ""
 
 let messageEl = document.getElementById("message-el")
@@ -26,7 +22,12 @@ function getRandomCard() {
 
 
 
-function startGame(){
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -51,11 +52,12 @@ function renderGame(){
     messageEl.textContent = message    
 } 
 
-function newCard(){
-    console.log("Drawing a new card from the deck!")
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+
 }
